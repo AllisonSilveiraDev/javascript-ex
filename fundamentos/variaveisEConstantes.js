1779f91539dd7945311540a55f54d142e1683079
Allison Silveira <allisonsilveiradev@gmail.com>
2023-02-15 16:08:27 -0300

    initial

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,6 @@
+// var escopo global pode ter seu valor reatribuido (pode causar erros futuros no codigo)
+var a = 3;
+// let escopo de bloco evita erros futuros, não pode ser valor reatribuido apenas alterado
+let b = 4;
+//const escopo de bloco seu valor não ser reatribuido ou alterado
+const c = 5;
